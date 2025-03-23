cc40bcb9c1591509469c8ed0f92fc49a8bc4034c
Elephant47 <vandinhdung2003@gmail.com>
2025-03-23 20:07:49 +0700

    Check Vercel

--- src/layouts/Header.tsx
+++ src/layouts/Header.tsx
@@ -11,6 +11,7 @@ function Header() {
 
    const { isAuthenticated, user, logout, isLoading} = useAuth();
    console.log(user)
+   ///prevent flickering
    if (isLoading) {
       return null
    }
