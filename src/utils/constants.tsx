490c0627f03ba4a00e5723b3146ed81794f2f0e0
Elephant47 <vandinhdung2003@gmail.com>
2025-03-24 01:55:55 +0700

    Check Vercel

--- src/utils/constants.tsx
+++ src/utils/constants.tsx
@@ -1,9 +1,9 @@
 // import 'dotenv/config'
 let apiRoot = ''
-console.log("Import meta env: ", import.meta.env)
-console.log("Process env: ", process.env)
-console.log("Import meta env: ", import.meta.env.API_ROOT)
-console.log("Process env: ", process.env.API_ROOT)
+// console.log("Import meta env: ", import.meta.env)
+// console.log("Process env: ", process.env)
+// console.log("Import meta env: ", import.meta.env.API_ROOT)
+// console.log("Process env: ", process.env.API_ROOT)
 if (process.env.BUILD_MODE === 'dev'){
     apiRoot = import.meta.env.VITE_URL_API
 }
